884e61fc55c261e424f72ac164423a5177924f28
qibin <qibin@inertia.local>
2018-02-26 11:36:50 +0800

    添加xml

--- routes/test.js
+++ routes/test.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var confirm=require('./confirm.js');
 var router = express.Router();
+var xml2js=require('xml2js');//微信推送消息是用的xml格式
 router.get('/', function (req, res, next) {
     console.log('测试服务器正常运行');
     var result=confirm.init(req);
@@ -17,6 +18,17 @@ router.get('/', function (req, res, next) {
 });
 router.post('/',function(req,res,next){
     //这是对推送消息进行处理。
+    var parser=new xml2js.Parser();
+    console.log(req);
+    // if(message.MsgType==="text")
+    // {
+    //     var content=message.Content;
+    //     var reply='你也好';
+    // }
+
+    // parser.parseString(data,function(err,result){
+
+    // })
     res.send('你好啊');
 })
 
