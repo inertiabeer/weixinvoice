47bd0eea252980828c3f96ce290b16d040e1f6e3
qibin <qibin@inertia.local>
2018-02-26 10:24:03 +0800

    添加加密

--- routes/test.js
+++ routes/test.js
@@ -1,8 +1,30 @@
-var express=require('express');
+var express = require('express');
+var crypto = require('crypto');
+var shasum = crypto.createHash('sha1');
 var router = express.Router();
-router.post('/', function (req, res, next) {
+router.get('/', function (req, res, next) {
     console.log('测试服务器正常运行');
-    res.send('yes');
+    //token 是weixin
+    //timestamp nonce token 按照字典排序
+    //
+    var timestamp = req.query.timestamp;
+    var nonce = req.query.nonce;
+    var token = 'weixin';
+    var echostr = req.query.echostr;
+    var signature = req.query.signature;
+    var testArray = [timestamp, nonce, token];
+    testArray.sort();
+    var temp = testArray.join('');
+    shasum.update(temp);
+    var sha1test = shasum.digest('hex');
+    is(sha1test == signature) {
+        res.send(echostr);
+    }
+    else {
+        console.log('验证出问题了');
+        res.send(echostr);
+    }
+
 });
 
 module.exports = router;
